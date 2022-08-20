b957976d42c7da0971fb6e78f4bb9319ccc23a00
arsany98 <arsanymaged13@gmail.com>
2022-08-20 22:30:28 +0200

    fix: a lot of changes to carousel - slide a whole page instead of one course - add event listeners to change courses count per page according to screen size - add no courses found text - hide nav arrows when there's no courses left

--- scripts.js
+++ scripts.js
@@ -24,32 +24,97 @@ async function fetchTracks() {
   let tracks = await response.json();
   return tracks;
 }
-function renderTracks(tracks) {
-  let tabsHTML = "";
-  tracks.forEach((track) => {
-    tabsHTML += `<li class="nav-item" role="presentation">
-    <button
-      class="tab mybtn"
-      id="pills-${track.id}-tab"
-      data-bs-toggle="pill"
-      data-bs-target="#pills-${track.id}"
-      type="button"
-      role="tab"
-      aria-controls="pills-${track.id}"
-      aria-selected="true"
-    >
-    ${track.name}
-    </button>
-  </li>`;
-  });
+function renderCourses(trackId, coursesHtml) {
+  let rendered = document.createElement("div");
+  if (coursesHtml.length === 0) {
+    rendered.innerHTML = `<div class="no-courses">No Results Found.</div>`;
+    return rendered.firstElementChild;
+  }
+  let minPerSlide;
+  if (window.innerWidth <= 576) minPerSlide = Math.min(1, coursesHtml.length);
+  else if (window.innerWidth <= 768)
+    minPerSlide = Math.min(2, coursesHtml.length);
+  else if (window.innerWidth <= 992)
+    minPerSlide = Math.min(3, coursesHtml.length);
+  else if (window.innerWidth <= 1200)
+    minPerSlide = Math.min(4, coursesHtml.length);
+  else minPerSlide = Math.min(6, coursesHtml.length);
 
-  let pillsTab = document.querySelector("#pills-tab");
-  pillsTab.innerHTML = tabsHTML;
+  let carouselItems = "";
+  for (let i = 0; i < coursesHtml.length; i++) {
+    if (i % minPerSlide === 0) {
+      if (i === 0) {
+        carouselItems += `<div class="carousel-item active">`;
+      } else {
+        carouselItems += `</div>`;
+        carouselItems += `<div class="carousel-item">`;
+      }
+    }
+    carouselItems += coursesHtml[i];
+  }
+  carouselItems += `</div>`;
+  rendered.innerHTML = `
+    <div id="CarouselControls${trackId}" class="carousel slide" data-bs-wrap="false">
+      <div class="carousel-inner">${carouselItems}</div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#CarouselControls${trackId}" data-bs-slide="prev">
+        <span class="material-icons-outlined md32">navigate_before</span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#CarouselControls${trackId}" data-bs-slide="next">
+        <span class="material-icons-outlined md32">navigate_next</span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>`;
+  let c = rendered.querySelector(".carousel");
+  let prevControl = c.querySelector(".carousel-control-prev");
+  let nextControl = c.querySelector(".carousel-control-next");
+  if (c.querySelector(".carousel-inner").childElementCount == 1)
+    nextControl.style.display = "none";
+  prevControl.style.display = "none";
+  c.addEventListener("slid.bs.carousel", (event) => {
+    let i = event.to;
+    let n = c.querySelector(".carousel-inner").childElementCount - 1;
+    if (i === 0) prevControl.style.display = "none";
+    else prevControl.style.display = "flex";
+
+    if (i === n) nextControl.style.display = "none";
+    else nextControl.style.display = "flex";
+  });
+  return rendered.firstElementChild;
+}
+function renderTracks(tracks) {
+  let tabs = document.querySelector("#pills-tab");
+  tabs.innerHTML = "";
   let tabContent = document.querySelector(".tab-content");
   tabContent.innerHTML = "";
   tracks.forEach((track) => {
-    renderTrack(track);
+    let tab = document.createElement("div");
+    tab.innerHTML = `
+    <li class="nav-item" role="presentation">
+      <button
+        class="tab mybtn"
+        id="pills-${track.id}-tab"
+        data-bs-toggle="pill"
+        data-bs-target="#pills-${track.id}"
+        type="button"
+        role="tab"
+        aria-controls="pills-${track.id}"
+        aria-selected="true"
+      >
+      ${track.name}
+      </button>
+    </li>`;
+    tab.querySelector("button").addEventListener("click", (event) => {
+      let currentTrack = document.querySelector(".tab-pane.active");
+      currentTrack.querySelector(".courses-container").innerHTML = "";
+      currentTrack
+        .querySelector(".courses-container")
+        .append(renderCourses(track.id, coursesToHtml(track.courses)));
+    });
+    tabs.append(tab.firstElementChild);
+    tabContent.append(renderTrack(track));
   });
+
   let firstTab = document.querySelector(".tab");
   firstTab.classList.add("active");
   let firstTabPane = document.querySelector(".tab-pane");
@@ -61,45 +126,9 @@ function renderTrack(track) {
   <button class="mybtn white-bg box bold-font">
     Explore ${track.name}
   </button>`;
-  let coursesHTML = "";
-  track.courses.forEach((course) => {
-    let ratingStars = renderStars(course.rating);
-    coursesHTML += `
-      <div class="carousel-item">
-        <article class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-            <a href="${course.url}">
-            <div class="img-container">
-              <div class="img-hover"></div>
-                <img 
-                  alt="${course.title}" 
-                  src="${course.image}" 
-                />
-              </div> 
-              <div>
-                <h3><b>${course.title}</b></h3> 
-                <p class="subtext-font">${course.author}</p> 
-                <div class="rating">
-                  <span class="rating-num"><b>${course.rating}</b></span>
-                    <span class="rating-stars">
-                      ${ratingStars}
-                    </span>
-                  <span class="subtext-font">(${course.rateCount.toLocaleString(
-                    "en-US"
-                  )})</span>
-                </div>
-                <div><b>E£${course.price.toLocaleString("en-US")}</b></div>
-                ${
-                  course.bestseller
-                    ? `<div class="best-seller"><b>Bestseller</b></div>`
-                    : ``
-                }
-              </div>
-            </a>
-          </article>
-        </div>`;
-  });
 
-  let trackHTML = `
+  let trackHTML = document.createElement("div");
+  trackHTML.innerHTML = `
   <div
     class="tab-pane track-container"
     id="pills-${track.id}"
@@ -108,40 +137,50 @@ function renderTrack(track) {
     tabindex="0"
   >
     <div class="track-head">${trackHeadHTML}</div>
-    <div class="row">
-      <div id="CarouselControls${track.id}" class="carousel slide">
-        <div class="carousel-inner courses-container">${coursesHTML}</div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#CarouselControls${track.id}" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#CarouselControls${track.id}" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
+    <div class="courses-container row"></div>
   </div>`;
-  let tabContent = document.querySelector(".tab-content");
-  tabContent.innerHTML += trackHTML;
-  let currentTrack = document.querySelector(".tab-pane:last-child");
-  let firstCourse = currentTrack.querySelector(".carousel-item");
-  if (firstCourse) firstCourse.classList.add("active");
-
-  let items = currentTrack.querySelectorAll(".carousel-item");
-
-  items.forEach((el) => {
-    const minPerSlide = Math.min(6, items.length);
-    let next = el.nextElementSibling;
-    for (var i = 1; i < minPerSlide; i++) {
-      if (!next) {
-        next = items[0];
-      }
-      let cloneChild = next.cloneNode(true);
-      el.appendChild(cloneChild.children[0]);
-      next = next.nextElementSibling;
-    }
+  trackHTML
+    .querySelector(".courses-container")
+    .append(renderCourses(track.id, coursesToHtml(track.courses)));
+  return trackHTML.firstElementChild;
+}
+function coursesToHtml(courses) {
+  let coursesHTML = [];
+  courses.forEach((course) => {
+    let ratingStars = renderStars(course.rating);
+    coursesHTML.push(`
+        <article class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+            <a href="${course.url}">
+            <div class="img-container">
+              <div class="img-hover"></div>
+              <img 
+                alt="${course.title}" 
+                src="${course.image}" 
+              />
+            </div> 
+            <div>
+              <h3><b>${course.title}</b></h3> 
+              <p class="subtext-font">${course.author}</p> 
+              <div class="rating">
+                <span class="rating-num"><b>${course.rating}</b></span>
+                  <span class="rating-stars">
+                    ${ratingStars}
+                  </span>
+                <span class="subtext-font">(${course.rateCount.toLocaleString(
+                  "en-US"
+                )})</span>
+              </div>
+              <div><b>E£${course.price.toLocaleString("en-US")}</b></div>
+              ${
+                course.bestseller
+                  ? `<div class="best-seller"><b>Bestseller</b></div>`
+                  : ``
+              }
+            </div>
+          </a>
+        </article>`);
   });
+  return coursesHTML;
 }
 function filterCourses(tracks, searchText) {
   let filtered = JSON.parse(JSON.stringify(tracks));
@@ -156,15 +195,29 @@ function filterCourses(tracks, searchText) {
   });
   return filtered;
 }
-let tracks;
 document.addEventListener("DOMContentLoaded", async (event) => {
-  tracks = await fetchTracks();
+  let tracks = await fetchTracks();
   renderTracks(tracks);
-});
-let form = document.querySelector("form");
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  let searchText = document.querySelector(".text-input");
-  let filtered = filterCourses(tracks, searchText.value);
-  renderTracks(filtered);
+
+  window.addEventListener("resize", (event) => {
+    let currentTrack = document.querySelector(".tab-pane.active");
+    let currentTrackName = document
+      .querySelector(".tab.active")
+      .textContent.trim();
+    let t = tracks.find((track) => {
+      return track.name === currentTrackName;
+    });
+    currentTrack.querySelector(".courses-container").innerHTML = "";
+    currentTrack
+      .querySelector(".courses-container")
+      .append(renderCourses(t.id, coursesToHtml(t.courses)));
+  });
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let searchText = document.querySelector(".text-input");
+    let filtered = filterCourses(tracks, searchText.value);
+    renderTracks(filtered);
+  });
 });
